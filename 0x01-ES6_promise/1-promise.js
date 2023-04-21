bf13c58d44a09b3ff7708e176cc19e8fa2de4e51
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2023-04-21 14:27:25 +0300

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getFullResponseFromAPI(success) {
         status: 200,
 	body: 'success',
       });
-    }else {
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
