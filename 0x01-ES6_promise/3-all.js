622adc0c2f4667639966d1b4f6776470af186a20
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2023-04-21 14:43:40 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto , createUser } from './utils';
-export default function handleProfileSignup(){
-	return Promise.all([uploadPhoto(), createUser()])
-	.then((resp) => {
-		console.log('${resp[0].body} ${resp[1].firstName} ${resp[1].lastName}');
-	})
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((resp) => {
+      console.log('${resp[0].body} ${resp[1].firstName} ${resp[1].lastName}');
+    })
 	.catch(() => console.log('Signup system offline'));
 }
